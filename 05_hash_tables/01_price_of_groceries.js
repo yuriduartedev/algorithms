366a5b1c778f2914c079af4aa3a720ed1ddfb0ac
Yuri Duarte <yuriduartedev@gmail.com>
2023-12-21 11:42:59 -0400

    hashtables chapther

---
+++ 05_hash_tables/01_price_of_groceries.js
@@ -0,0 +1,7 @@
+const book = {};
+
+book["apple"] = 0.67;
+book["milk"] = 1.49;
+book["avocado"] = 1.49;
+
+console.log(book); // { apple: 0.67, milk: 1.49, avocado: 1.49 }
---
+++ 05_hash_tables/02_check_voter.js
@@ -0,0 +1,18 @@
+const voted = {};
+
+function check_voter(name) {
+  if (voted[name]) {
+    console.log("kick them out!");
+  } else {
+    voted[name] = true;
+    console.log("let them vote!");
+  }
+}
+
+check_voter("tom"); // let them vote!
+check_voter("mike"); // let them vote!
+check_voter("mike"); // kick them out!
+
+console.group("voted");
+console.log(voted);
+console.groupEnd();
